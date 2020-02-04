08c0ccee192ad537801fbdea34029a0e35405bde
Jonathan <jonathan.shifman@gmail.com>
2020-02-04 20:38:07 +0200

    Set up React

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.module.css';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return 'aaa';
+    }
+}
+
+export default App;
